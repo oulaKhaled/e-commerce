3553a3615d2174bd7ff1a8e344eaa3ffa6f31bd8
oulaKhaled <oulakh23@gmail.com>
2024-06-21 15:55:54 +0300

    edit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -32,7 +32,7 @@ function Header({sendDataToHome}){
   const[Address,setAddress]=useState("");
   const[confirmPassword,setConfirmPassword]=useState("");
   const [profileID,getProfileID]=useState("");
-
+const [token,SetToken]=useState("");
   const [currentUser,setCurrentUser]=useState(false)
 const [profile,setProfile]=useState([]);
 const [query,setQuery]=useState([]);
@@ -76,19 +76,35 @@ const handelClick=()=>{
   sendDataToHome(query)
 }
 
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
 
 const getToken= async()=>{
-  try{
-    let response = await axios.get(`${BASE_URL}/app/csrf/`);
-    console.log(response.data);
-
-  } 
-  catch(error){
-    console.log(error);
-  }
-  
+  // try{
+  //   let response = await axios.get(`${BASE_URL}/app/csrf/`);
+  //   console.log(response.data);
+  //   SetToken(response.data);
+  // } 
+  // catch(error){
+  //   console.log(error);
+  // }
+  console.log("IGOT THİS TOKEN : ",csrftoken);
+  console.log("COOKİE : ",document.cookie);
 }
-
 
 
 const UpdateProfile= async(e)=>{
@@ -135,7 +151,7 @@ catch(error){
       withCredentials:true,
       headers:{ 
         'Content-Type': 'application/json',
-        // "X-CSRFToken":csrftoken
+        "X-CSRFToken":token
       },
     });
      
@@ -205,7 +221,7 @@ useEffect(()=>{
 },[profile])
 useEffect(()=>{
 
-  check_auth()
+ 
  
 },[])
 
@@ -214,6 +230,7 @@ useEffect(()=>{
     return (
         <Navbar className=" justify-content-between" style={{"backgroundColor":"#152A38",width:"100%"}}>
 <button onClick={getToken}>Get token</button>
+<button onClick={check_auth}>check auth</button>
    
         <Form inline >
       
